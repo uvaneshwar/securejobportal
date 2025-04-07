849339f8c0c75326c2132cf26e63eeeb64b5ef8a
Uvaneshwar S <126236924+uvaneshwar@users.noreply.github.com>
2025-04-07 19:15:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 
 // Serve index.html from public directory
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Registration endpoint
